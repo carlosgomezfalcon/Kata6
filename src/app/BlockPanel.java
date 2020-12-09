ad04f5374a93cbd1bb6b9d157b67eec8809887ee
Carlos <Carlos@Carlos-Gmez-OMEN>
2020-12-09 14:33:15 +0000

    Kata6 versión 1

---
+++ src/app/BlockPanel.java
@@ -0,0 +1,42 @@
+package app;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import model.Block;
+import view.BlockDisplay;
+
+class BlockPanel extends JPanel implements BlockDisplay {
+    private static final int SIZE = 100;
+    private Block block;
+    
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        
+        int d = Block.MAX * SIZE;
+        g.setColor(Color.black);
+        for (int i = 0; i <= Block.MAX; i++) {
+            int c = i*SIZE;
+            g.drawLine(0, c, d, c);
+            g.drawLine(c, 0, c, d);
+        }
+        
+        if(block == null) return;
+        g.setColor(Color.red);
+        g.fillRect((block.x()-1)*SIZE, (Block.MAX - block.y())*SIZE, SIZE, SIZE);
+    }
+
+    @Override
+    public void display(Block block) {
+        this.block = block;
+        repaint();
+    }
+
+    @Override
+    public void changed() {
+        repaint();
+    }
+    
+}
---
+++ src/view/BlockDisplay.java
@@ -0,0 +1,7 @@
+package view;
+
+import model.Block;
+
+public interface BlockDisplay extends Block.Observer{
+    void display(Block block);
+}
